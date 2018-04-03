4ce54ff26fc8479e290a336d2d3a763b16942659
Ariel Azarcon <aazarcon@ad.acquireap>
2018-04-03 19:02:05 +0800

    pricing, security and why ibinex markup done. Sass to be converted yet.

---
+++ resources/views/pricing/index.blade.php
@@ -0,0 +1,103 @@
+@extends('layouts.app')
+@section('title','Pricing')
+@push('stylesheets')
+    <link rel="stylesheet" href="{{ asset('css/contact.css') }}">
+@endpush
+
+@section('content')
+    <div class="container-fluid">
+        <section id="pricing-section">
+            <div>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <img src="../First-option-Pricing.jpg">
+                    </div>
+
+                    <div class="col-lg-3" id="pricing-text">
+                        <div class="row">
+                            <article class="col-lg-12">
+                                <h2>Pricing</h2>
+                                <p>
+									<span>
+										Integration of the Ibinex turnkey cryptocurrency platform to ex­change your way requires:
+									</span>
+                                </p>
+
+                                <ul>
+                                    <li>$150,000 "reserve only, no pay­ment" deposited in a local Escrow account of your choice for 180 days</li>
+                                    <li>Your first 90 days of Ibinex are gratis while you build your book.</li>
+                                </ul>
+
+
+                            </article>
+                        </div>
+
+                        <div class="row">
+                            <article class="col-lg-12">
+                                <h2>Our fees</h2>
+                                <p>
+									<span>
+										0.1% flat-rate fee based on exchange volume with a minimum fee of $20,000 per month
+									</span>
+                                </p>
+
+                                <p>
+									<span>
+										Fees begin only at day 91.
+									</span>
+                                </p>
+
+
+                            </article>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="setup-onboarding-section">
+
+            <div class="text-content">
+                <div class="row">
+
+                    <article id="setup-article" class="col-lg-6">
+
+                        <h2>Setup</h2>
+                        <p>
+								<span>
+									In order to proceed with setup, we require:
+								</span>
+                        </p>
+                        <p>
+								<span>
+									Your company to sign out Letter of Intent (LOI)
+								</span>
+                        </p>
+                        <p>
+								<span>
+									Proof of the Escrow deposit at a bank or with an attorney of your choice.
+								</span>
+                        </p>
+                    </article>
+
+                    <article class="col-lg-6" id="onboarding-article">
+
+                        <h2>Onboarding time</h2>
+                        <p>
+								<span>
+									Please note that, due to incredibly high demand of the Ibinex exchange solutions suite, the current onboarding waiting time is 4 months.
+								</span>
+                        </p>
+                        <p>
+								<span>
+									We are doing our utmost to reduce time to market while retaining our trademark standard of excellence, and appreciate your interest in our innovations.
+								</span>
+                        </p>
+
+                    </article>
+                </div>
+            </div>
+        </section>
+    </div>
+@endsection
