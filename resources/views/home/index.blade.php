ecc5966ed0c0042701a99889737a384d71313556
Melvin San Jose <msanjose@ad.acquireap>
2018-04-05 11:03:30 +0800

    Add homepage placeholder

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+<iframe src="https://giphy.com/embed/IB9foBA4PVkKA" width="480" height="474" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/dancing-dance-excited-IB9foBA4PVkKA">via GIPHY</a></p>
+
+@endsection
