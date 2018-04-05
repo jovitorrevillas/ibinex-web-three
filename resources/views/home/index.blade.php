6fa912b898d52eb064a7a2f79b3589e87de90042
Melvin San Jose <melvin.sj@ibinex.tech>
2018-04-05 12:03:52 +0800

    Edit placeholder index

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-<iframe src="https://giphy.com/embed/IB9foBA4PVkKA" width="480" height="474" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/dancing-dance-excited-IB9foBA4PVkKA">via GIPHY</a></p>
-
+<h3 style="text-align: center;padding: 10%;">This content is not available in your location.</h3>
 @endsection
