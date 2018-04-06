0baf492525e5fab2b9bcaf7b2e3bce7c1c752ca4
mscxyz <mychachiiii@gmail.com>
2018-04-06 13:55:59 +0800

    fresh

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -5,86 +5,99 @@
 @endpush
 
 @section('content')
-    <div id="contact-us-content">
-        <section class='first-fold' id='getStarted'>
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-sm-12 get-started-text">
-                        <h1>Get Started</h1>
-                        <p>
-                            We invite you to first experience the Ibinex demo here, which will help you to conceptualise the possibilities of personalisation for your own exchange.
-                        </p>
-                        <p>
-                            The next step is to contact us to discuss on-boarding. It is a simple process which you can learn about in more detail on the Pricing page, but essentially consists of 3 steps:
-                        </p>
-                        <ul>
-                            <li>Local Escrow deposit (reserve only, no payment)</li>
-                            <li>Letter of Intent</li>
-                            <li>Proof of Escrow deposit</li>
-                        </ul>
-                        <p>Please note that, due to high demand, the current on-boarding timeframe is 4 months.</p>
-                    </div>
-                </div>
-            </div>
-        </section> <!-- getStarted -->
+    <div  id="getStarted">
 
-        <section class='second-fold' id='support'>
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-sm-4 support-div">
-                        <h1>Support</h1>
-                        <p>
-                            Contact and feedback are integral to the Ibinex way, enabling us to cater to your needs and requirements effectively and efficiently.
-                        </p>
-                        <p>
-                            For support regarding any aspect of the Ibinex platform, your use of this website or your own exchange integration, please contact us with your enquiry for personalised customer service.
-                        </p>
-                        <p>
-                            We will endeavour to respond to you as promptly as possible.
-                        </p>
-                        <form class="form-mobile">
-                            <div class="form-group">
-                                <label for="fullname">Full Name</label>
-                                <input type="text" class="form-control" id="fullname" required="">
-                            </div>
-                            <div class="form-group">
-                                <label for="emailAdd">Email Address</label>
-                                <input type="text" class="form-control" id="emailAdd" required="">
-                            </div>
-                            <div class="form-group">
-                                <label for="emailSubject">Subject</label>
-                                <input type="text" class="form-control" id="emailSubject" required="">
-                            </div>
-                            <div class="form-group">
-                                <label for="emailMessage">Message</label>
-                                <textarea class="form-control" id="emailMessage" required=""></textarea>
-                            </div>
-                            <button type="submit" class="btn btn-lg">SEND</button>
-                        </form>
-                    </div>
-                    <div class="box-floated not-mobile">
-                        <form class="">
-                            <div class="form-group">
-                                <label for="fullname">Full Name</label>
-                                <input type="text" class="form-control" id="fullname" required="">
-                            </div>
-                            <div class="form-group">
-                                <label for="emailAdd">Email Address</label>
-                                <input type="text" class="form-control" id="emailAdd" required="">
-                            </div>
-                            <div class="form-group">
-                                <label for="emailSubject">Subject</label>
-                                <input type="text" class="form-control" id="emailSubject" required="">
-                            </div>
-                            <div class="form-group">
-                                <label for="emailMessage">Message</label>
-                                <textarea class="form-control" id="emailMessage" required=""></textarea>
-                            </div>
-                            <button type="submit" class="btn btn-lg">SEND</button>
-                        </form>
-                    </div>
+            <div class="container">
+                    <div class="row">
+                
+                <div class="col-md-6">
+                    
+
+        <h1 class="get-started-title title-heading">Get Started</h1>
+
+            <p class="p1">We invite you to first experience the Ibinex demo here, which will help you to conceptualise the possibilities of personalisation for your own exchange.</p>
+
+            <p class="p2">The next step is to contact us to discuss on-boarding. It is a simple process which you can learn about in more detail on the Pricing page, but essentially consists of 3 steps:</p>
+
+
+            <ul class="check-items">
+                <li><span>Local Escrow deposit (reserve only, no payment)</span></li>
+                <li><span>Letter of Intent</span></li>
+                <li><span>Proof of Escrow deposit</span></li>
+            </ul>
+
+            <p class="note">Please note that, due to high demand, the current on-boarding timeframe is 4 months.</p>
+
                 </div>
+        </div>
+            
             </div>
-        </section> <!-- support -->
+ 
     </div>
+
+
+    <div id="support" class="container-fluid">
+        <div class="container" >
+            
+            <div class="row ">
+            <div class="col-md-6" id="support-description">
+                    
+
+                <h1 class="support-title title-heading">Support</h1>
+
+
+                <p class="p3">Contact and feedback are integral to the Ibinex way, enabling us to cater to your needs and requirements effectively and efficiently.</p>
+
+                <p class="p4">For support regarding any aspect of the Ibinex platform, your use of this website or your own exchange integration, please contact us with your enquiry for personalised customer service.</p>
+
+
+                <p class="p5">We will endeavour to respond to you as promptly as possible.</p>
+            
+                
+
+            </div>
+
+            <div class="col-md-6" id="support-right">
+                
+
+                <form >
+                  <div class="form-group">
+                    <label for="fullname">Full Name</label>
+                    <input type="text" class="form-control" >
+                  
+                  </div>
+
+                  <div class="form-group">
+                    <label for="email">Email Address</label>
+                    <input type="password" class="form-control" >
+                  </div>
+
+                   <div class="form-group">
+                    <label for="subject">Subject</label>
+                    <input type="text" class="form-control" >
+                  </div>
+
+                    <div class="form-group">
+
+                    <label for="message">Message</label>
+                    <textarea class="form-control" rows="10"></textarea>
+
+                  </div>
+                
+                  <button type="submit" class="btn-primary" >SEND</button>
+                </form>
+
+
+            </div>
+
+        </div>
+
+        </div>
+        
+
+    </div>
+
+
+
+
 @endsection
---
+++ resources/views/services/index.blade.php
@@ -0,0 +1,223 @@
+@extends('layouts.app')
+@section('title','Contact Us')
+@push('stylesheets')
+    <link rel="stylesheet" href="{{ asset('css/services.css') }}">
+@endpush
+
+@section('content')
+
+
+
+	
+
+	<div class="jumbotron services text-center"  >
+			
+			<h1 class="services-title" style="font-style: italic;font-family: 'Montserrat', sans-serif;">Services</h1>
+
+			<p id="ibinex_services">Ibinex services are all characterised by high-level innovation made practical for your personalised <br>exchange integration.</p>
+			<p id="ibinex_ecosystem">Our ecosystem of services comprises of the following units:</p>
+
+
+	</div>
+
+
+	<div class="container" id="platform">
+
+		  	<div class="row">
+
+				    <div class="col-md-6" >
+				    	
+				 		   <img src="img/services/Ibinex-Platform2.png" class="platform-laptop" >
+
+				    </div>
+
+				    <div class="col-md-6 ">
+
+				    <h1 class="heading platform-heading">Platform</h1>
+
+				  	 <p class="platform-description">Your exchange is your launchpad. Our suite elevates your exchange by equipping you with your own platform consisting of:</p>
+
+
+				  	 <ul class="check-items ">
+				  	 	<li> <span>Tailor-made private, institutional while label infrastructure</span></li>
+				  	 	<li>
+				  	 		<span>Fully-customisable front-end website</span></li>
+				  	 	<li><span>Completely integrated web-based trading platform</span></li>
+				  	 	<li><span>Flexible front-end user trading</span></li>
+				  	 	<li><span>Performance-driven features</span></li>
+				  	 	<li><span>Secure private cloud hosting</span></li>
+				  	 	<li><span>Total compatibility with your own branding deck.</span></li>
+				  	 </ul>
+				  	
+
+				    </div>
+
+		  </div>
+	</div>
+		
+	
+
+
+	<div class="container" id="crm">
+		
+			
+				<div class="row" style="padding:60px;">
+
+					   <div class="col-xl-6">
+
+					   	<h1 class="heading crm-heading">CRM/Back Office</h1>
+
+				  	 <p class="crm-description">The Ibinex solution unites the front-end and platform with a full back office suit including CRM with:</p>
+
+
+
+				  	 <ul class="crm-check-items">
+				  	 	<li>
+				  	 	<span>KYC/AML documentation collection</span>
+				  	 	</li>
+				  	 	<li><span>Documentation verification</span></li>
+				  	 	<li><span>Account and money management</span></li>
+				  	 	<li><span>Optimised back-office administrative workflow</span></li>
+				  	 	<li><span>Enhanced UI/UX</span></li>
+				  	 </ul>
+
+				  	 	
+						
+						
+						
+						
+
+			
+
+				    </div>
+
+
+				    <div class="col-xl-6">
+				    
+				    	<img src="img/services/docs.png" class="img-fluid crm-dashboard">
+
+				    </div>
+
+				 
+
+		 	 </div>
+
+
+
+	</div>
+
+
+	<div class="cryptobridge">
+		
+				
+				<div class="container">
+					
+					<div class="row">
+
+					   <div class="col-xl-6">
+
+						
+					   	<img src="img/services/cryptobridge.png" class="img-fluid c3-logo" >
+				
+
+				   		</div>
+
+
+				    <div class="col-xl-6 col-xs-12">
+
+				    	<h1 class="heading crypto-heading">CryptoBridge™</h1>
+
+				    	<p class="crypto-descritpion">Aggregate elite exchanges and indexes as the cryptocurrency market moves, with out patent-pending proprietary software suite, CryptoBridge™. Gain access to 60 premier exchanges for maximum liquidity cryptocurrencies, all available only through Cryptobridge™ by Ibinex.</p>
+
+				    </div>
+
+				 
+
+		 	 	</div>
+
+				</div>
+
+				
+
+
+
+	</div>
+
+
+	<div class="container ccp" >
+		
+			
+				<div class="row" >
+
+					   <div class="col-xl-6  ">
+
+						
+						<h1 class="heading ccp-heading">Credit Card Processing</h1>
+
+				    	<p class="ccp-description">We offer full spectrum payment processing for cryptocurrencies, and a complete selection that is unavailable elsewhere. Exchanging your way with Ibinex means experiencing a whole new world of payment options including: credit cards, debit cards, wire transfers, alternative payments, hosted wallets and cold wallets.</p>
+
+				    	<img src="img/services/cards.png" class="img-fluid ccp-cards" >
+			
+
+				   		</div>
+
+
+				    <div class="col-xl-6">
+
+				    	<img src="img/services/creditcard-phone.png" class="img-fluid ccp-phone">
+
+
+
+				    </div>
+
+				 
+
+		 	 </div>
+
+
+
+	</div>
+
+
+	<div  style="background: #fbfbfb;" id="ca">
+		
+					<div class="container">
+						
+						<div class="row">
+
+					   <div class="col-xl-6">
+
+						
+						<img src="img/services/custodian_accounts.png" class="img-fluid float-left ca-person">
+			
+
+				   		</div>
+
+				    <div class="col-xl-6" style="padding-top:120px;">
+
+				    			<h1 class="heading ca-heading">Custodian Accounts</h1>
+
+				    	<p class="ca-description">Another first sees Ibinex leading the way as the world's first cryptocurrency exchange with a finance transaction license. We are proud to be the first to offer you:</p>
+
+				    	<ul class="ca-items">
+				    		<li>Fully-compliant and licensed custodian proceeding and accounts</li>
+				    		<li>The choice to instead select a custodian of your choice</li>
+				    		<li>Payments via swipe-and-transmit from FIAT to base currency</li>
+				    		<li>A raft of other custodian account options unavailable elsewhere</li>
+				    	</ul>
+
+				    </div>
+
+				 
+
+		 	 </div>
+
+					</div>
+				
+
+
+
+	</div>
+
+
+@endsection
