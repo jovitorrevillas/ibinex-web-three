f325bcd5b1a749928df51ee513da25223c00b56b
Ariel Azarcon <aazarcon@ad.acquireap>
2018-04-10 16:02:39 +0800

    [Security] desktop (1920px). css of last commit not saved.

--- resources/views/security/index.blade.php
+++ resources/views/security/index.blade.php
@@ -8,7 +8,7 @@
     <div id="security" class="container-fluid">
         <section id="security-section">
             <div class="row">
-                <div id="bg-blue" class="col-lg-12 col-md-12">
+                <div id="bg-blue" class="col-lg-12 col-12">
 
                     <div class="text-content">
 
@@ -84,7 +84,7 @@
                 <div class="text-content">
                     <div class="row">
 
-                        <div class="col-lg-6 col-md-12">
+                        <div class="col-lg-6 col-12">
                             <h3>Information Security</h3>
 
                             <p>
@@ -95,7 +95,7 @@
 
                         </div>
 
-                        <div class="col-lg-6 col-md-6">
+                        <div class="col-lg-6 col-6">
                             <h3>Privacy</h3>
 
                             <p>
