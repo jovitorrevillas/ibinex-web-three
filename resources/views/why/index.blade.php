1012040b34dd72bca58d90772ae686b4a68ffb89
John Jeevon Ang <jang@ad.acquireap>
2018-04-12 14:49:58 +0800

    why ibinex update

--- resources/views/why/index.blade.php
+++ resources/views/why/index.blade.php
@@ -35,38 +35,46 @@
                 </div>
             </div>
         </div>
-        <div class="container container-custom">
-            <div id="ib-why-tsec-text-wrapper">
-                <p id="ib-why-tsec-text-title">Why you should exchange your way <span>with Ibinex:</span></p>
-                <p id="ib-why-tsec-text-content">Our strategy is underpinned with the 4 core strengths of Ibinex:</p>
+        <div class="bordered-container">        
+            <div class="container container-custom">
+                <div id="ib-why-tsec-text-wrapper">
+                    <p id="ib-why-tsec-text-title">Why you should exchange your way <span>with Ibinex:</span></p>
+                    <p id="ib-why-tsec-text-content">Our strategy is underpinned with the 4 core strengths of Ibinex:</p>
+                </div>
+                <div class="container container-custom">
+                    <div class="row">
+                        <div id="ib-why-core-innovation-wrapper" class="col-lg-6 col-xl-6 ib-core">
+
+                            <img id="ib-why-core-innovation-logo" class="img-fluid" src="{{ asset('images/why/innovation.png') }}">
+                            <p id="ib-why-core-innovation-text" class="ib-core-text">Commitment to technological innovation,<br>
+                                through out patent-pending proprietary<br>
+                                software, Cryptobridge&trade;</p>
+                        </div>
+                        <div id="ib-why-core-platform-wrapper" class="col-lg col-xl offset-lg-1 ib-core">
+                            <img id="ib-why-core-platform-logo" class="img-fluid" src="{{ asset('images/why/platform.png') }}">
+                            <p id="ib-why-core-platform-text" class="ib-core-text">Trailblazing of complete and<br>
+                                customisable white label platform<br>
+                                solutions</p>
+                        </div>
+                        <div id="ib-why-core-service-wrapper" class="col-lg-6 col-xl-6 ib-core">
+                            <img id="ib-why-core-service-logo" class="img-fluid" src="{{ asset('images/why/service.png') }}">
+                            <p id="ib-why-core-service-text" class="ib-core-text">Offering unprecedented custodian service<br>
+                                with our industry's first finance transaction<br>
+                                license</p>
+                        </div>
+                        <div id="ib-why-core-payment-wrapper" class="col-lg col-xl offset-lg-1 ib-core">
+                            <img id="ib-why-core-payment-logo" class="img-fluid" src="{{ asset('images/why/payment.png') }}">
+                            <p id="ib-why-core-payment-text" class="ib-core-text">Providing a full-spectrum payment<br>
+                                processing infrastracture to revolutionise<br>
+                                liquidity for your exchange.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
+        </div>
+        <div class="container container-custom">
             <div class="container container-custom">
                 <div class="row">
-                    <div id="ib-why-core-innovation-wrapper" class="col-lg-6 col-xl-7 ib-core">
-
-                        <img id="ib-why-core-innovation-logo" class="img-fluid" src="{{ asset('images/why/innovation.png') }}">
-                        <p id="ib-why-core-innovation-text" class="ib-core-text">Commitment to technological innovation,<br>
-                            through out patent-pending proprietary<br>
-                            software, Cryptobridge&trade;</p>
-                    </div>
-                    <div id="ib-why-core-platform-wrapper" class="col-lg-6 col-xl ib-core">
-                        <img id="ib-why-core-platform-logo" class="img-fluid" src="{{ asset('images/why/platform.png') }}">
-                        <p id="ib-why-core-platform-text" class="ib-core-text">Trailblazing of complete and<br>
-                            customisable white label platform<br>
-                            solutions</p>
-                    </div>
-                    <div id="ib-why-core-service-wrapper" class="col-lg-6 col-xl-7 ib-core">
-                        <img id="ib-why-core-service-logo" class="img-fluid" src="{{ asset('images/why/service.png') }}">
-                        <p id="ib-why-core-service-text" class="ib-core-text">Offering unprecedented custodian service<br>
-                            with our industry's first finance transaction<br>
-                            license</p>
-                    </div>
-                    <div id="ib-why-core-payment-wrapper" class="col-lg-6 col-xl ib-core">
-                        <img id="ib-why-core-payment-logo" class="img-fluid" src="{{ asset('images/why/payment.png') }}">
-                        <p id="ib-why-core-payment-text" class="ib-core-text">Providing a full-spectrum payment<br>
-                            processing infrastracture to revolutionise<br>
-                            liquidity for your exchange.</p>
-                    </div>
                     <div id="ib-why-vision" class="col-lg-8 offset-lg-2 col-xl-8 offset-xl-2">
                         <p>This vision of a balanced yet pioneering approach to blockchain technology
                         software and white label platform tunkey solutions, aims to maximise liquidity
